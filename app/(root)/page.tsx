6ae299a467eb06399e32036971e844b35ce00349
Costa Ilisescu <coste_ilisescu@yahoo.com>
2025-01-04 15:39:50 +0000

    Finish base hero section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -13,7 +13,29 @@ export default function Home() {
         />
         <span className="absolute top-0 bottom-0 w-full h-full bg-black-300/80 z-20"></span>
         <div className="absolute inset-0 z-20 flex items-center justify-center text-white">
-          <h1 className="text-gray text-6xl pt-28 z-30">Home Title</h1>
+          <div className="flex gap-6">
+            <h2 className="text-gray text-4xl sm:text-5xl md:text-7xl z-30 hero-text-box-width text-right">
+              Elevate
+            </h2>
+            <Image
+              src="/hero-rectangle.png"
+              alt="A colorful rectangle"
+              width={77}
+              height={583}
+              className="h-[70vh] w-10 md:w-full"
+            />
+            <div className="flex flex-col items-baseline justify-end hero-text-box-width">
+              <h2 className="text-gray text-4xl sm:text-5xl md:text-6xl z-30">
+                Your
+              </h2>
+              <h2 className="text-purple text-4xl sm:text-5xl md:text-6xl z-30">
+                Online
+              </h2>
+              <h2 className="gradient-neptune text-4xl sm:text-5xl md:text-7xl z-30">
+                Presence
+              </h2>
+            </div>
+          </div>
         </div>
       </section>
     </>
