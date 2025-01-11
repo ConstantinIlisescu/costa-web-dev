df26eff02acfed0e2966976afaf8d8441488aabd
Costa Ilisescu <coste_ilisescu@yahoo.com>
2025-01-11 15:35:59 +0000

    Reduce the navbar button animation time

--- components/NavbarMenuBtn.tsx
+++ components/NavbarMenuBtn.tsx
@@ -16,17 +16,17 @@ const NavbarMenuBtn = ({
       <div className="flex flex-col gap-2 items-end">
         <div
           className={`w-20 h-1 bg-orange rounded-xl transition-all duration-500 ease-in-out ${
-            menuOpen ? "w-96 opacity-0" : "delay-300"
+            menuOpen ? "w-96 opacity-0" : "delay-100"
           }`}
         ></div>
         <div
           className={`w-16 h-1 bg-orange rounded-xl transition-all duration-500 ease-in-out ${
-            menuOpen ? "w-80 opacity-0" : "delay-300"
+            menuOpen ? "w-80 opacity-0" : "delay-100"
           }`}
         ></div>
         <div
           className={`w-10 h-1 bg-orange rounded-xl transition-all duration-500 ease-in-out ${
-            menuOpen ? "w-72 opacity-0" : "delay-300"
+            menuOpen ? "w-72 opacity-0" : "delay-100"
           }`}
         ></div>
       </div>
--- components/NavbarMenuDesktop.tsx
+++ components/NavbarMenuDesktop.tsx
@@ -4,7 +4,7 @@ import NavbarLinks from "./NavbarMenuLinks";
 const NavbarMenuDesktop = ({ menuOpen }: { menuOpen: boolean }) => {
   return (
     <div
-      className={`hidden md:block transition-all duration-1000 ease-in-out ${
+      className={`hidden md:block transition-all duration-500 ease-in-out ${
         menuOpen ? "delay-100 translate-x-0" : "translate-x-96"
       }`}
     >
